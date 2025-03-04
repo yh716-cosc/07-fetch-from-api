b8798c36469981750dbc3534cd474d28376d06af
Guil H <g.hernandez07@gmail.com>
2025-03-04 20:09:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select the button and joke container elements from the HTML
+const jokeBtn = document.getElementById('joke-btn');
+const setup = document.getElementById('setup');
+const punchline = document.getElementById('punchline');
+
+// Add an event listener to the button
+jokeBtn.addEventListener('click', function() {
+  console.log('Getting a joke...');``
+});

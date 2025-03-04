06c582248351a68f84e317565684ed11f61e1fd8
Guil H <g.hernandez07@gmail.com>
2025-03-04 21:33:35 +0000

    Final project

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ const punchline = document.getElementById('punchline');
 // Add an event listener to the button
 jokeBtn.addEventListener('click', function() {
   console.log('Getting a joke...');
+  
+  // Fetch a random programming joke from the Official Joke API
+  fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+    .then(response => response.json()) // Parse the JSON from the response
+    .then(data => {
+      // Display the joke on the page
+      setup.textContent = data[0].setup;
+      punchline.textContent = data[0].punchline;
+    });
 });

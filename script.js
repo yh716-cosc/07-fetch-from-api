40f7f88cb4fef9ad8f40f8ea20bfd02ee48a7fcd
Yixuan Huang <yh716@georgetown.edu>
2025-06-30 07:30:11 +0000

    fetch programming joke

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ const punchline = document.getElementById('punchline');
 
 // Add an event listener to the button
 jokeBtn.addEventListener('click', function() {
-  console.log('Getting a joke...');
+  console.log('Getting a programming joke...');
+  
+  // Fetch a random programming joke from the Official Joke API
+  fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+    .then(function(response) {
+      // Convert the response to JSON
+      return response.json();
+    })
+    .then(function(data) {
+      // The API returns an array with one joke object
+      const joke = data[0];
+      // Display the joke setup and punchline on the page
+      setup.textContent = `Setup: ${joke.setup}`;
+      punchline.textContent = `Punchline: ${joke.punchline}`;
+    });
 });
